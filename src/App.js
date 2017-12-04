1abaf1b4a25c0cf3c290154477b7e8e1fdf2581b
Pablo Tabares Hernandez <pablother3@gmail.com>
2017-12-03 21:30:00 -0500

    installed babel

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  const element = <h2>Hola Pablo</h2>
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Pablo</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
