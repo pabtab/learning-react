891e65a70d30111cae537bf9e55281e6af93476e
Pablo Tabares Hernandez <pablother3@gmail.com>
2017-12-03 22:16:48 -0500

    split in components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Header, Body } from "./Routing";
 
 class App extends Component {
-
-  const element = <h2>Hola Pablo</h2>
-  
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Pablo</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <div>
+      <Header></Header>
+      <Body></Body>
+    </div>
     );
   }
 }
---
+++ src/Routing.js
@@ -0,0 +1,4 @@
+import Header from './components/header/header';
+import Body from './components/body/body';
+
+export {Header, Body}
